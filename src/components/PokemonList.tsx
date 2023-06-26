b8f347c76922f6d9529a20655f4963b3b9a3675b
arnau.merino <arnau.merino@csuc.cat>
2023-06-26 12:10:43 +0200

    Frontend inicial

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import usePokemons from "../hooks/usePokemons";
+import "./PokemonList.css";
 
 export default function PokemonList() {
+  const [searchTerm, setSearchTerm] = useState("");
   const pokemons = usePokemons();
 
-  const pokemonItems = pokemons.map((pokemon, i) => (
-    <li key={i}>
-      <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
-    </li>
-  ));
+  const pokemonItems = pokemons
+    .filter((pokemon) => pokemon.name.includes(searchTerm.toLowerCase()))
+    .map((pokemon, i) => (
+      <div key={i} className="pokemon-list-item">
+        <Link to={`/pokemon/${pokemon.name}`}>
+          <span>{pokemon.name}</span>
+        </Link>
+      </div>
+    ));
 
-  return <ul>{pokemonItems}</ul>;
+  return (
+    <div className="pokemon-list-container">
+      <h1 className="pokemon-list-header">Pokemon List</h1>
+      <div className="pokemon-search">
+        <input
+          type="text"
+          placeholder="Search for Pokemon..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button>Search</button>
+      </div>
+      <div className="pokemon-list">{pokemonItems}</div>
+    </div>
+  );
 }
